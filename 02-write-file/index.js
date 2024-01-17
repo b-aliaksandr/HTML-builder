143c5740e0cef92695986c36f8248773a57c480c
barodzichaliaskandr@gmail.com <borodichalexander97@gmail.com>
2024-01-17 11:44:56 -0700

    feat(task-02): handle stop process

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -3,16 +3,28 @@ const path = require('node:path');
 const { stdout, stdin } = require('node:process');
 const readline = require('node:readline/promises');
 
+const EXIT_PHRASE = 'exit';
+
 const fileName = 'text.txt';
 const welcomeMessage = 'Hi, student \n';
+const farewellMessage = 'Bye';
 
 const writableStream = fs.createWriteStream(path.join(__dirname, fileName));
 const rl = readline.createInterface({ input: stdin, output: writableStream });
 
 const handleReadLine = (line) => {
+  if (line === EXIT_PHRASE) {
+    process.emit('SIGINT');
+  }
   rl.output.write(line + '\n');
+};
+
+const handleExit = () => {
+  stdout.write(farewellMessage);
+  process.kill(process.pid, 'SIGINT');
 };
 
 writableStream.on('open', () => stdout.write(welcomeMessage));
 
 rl.on('line', handleReadLine);
+process.on('SIGINT', handleExit);
