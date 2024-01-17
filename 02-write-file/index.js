988f48b5259bdcd1209e3fb06b908e7d1e37dda1
barodzichaliaskandr@gmail.com <borodichalexander97@gmail.com>
2024-01-17 09:46:03 -0700

    feat(task-02): add welcome message output

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,6 +1,10 @@
 const fs = require('node:fs');
 const path = require('node:path');
+const { stdout } = require('node:process');
 
 const fileName = 'text.txt';
+const welcomeMessage = 'Hi, student';
 
 const writableStream = fs.createWriteStream(path.join(__dirname, fileName));
+
+writableStream.on('open', () => stdout.write(welcomeMessage));
