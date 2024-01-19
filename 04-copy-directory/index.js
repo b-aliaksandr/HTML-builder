d1c63ee5414e77927236dbc0bac3b34bee00c82d
barodzichaliaskandr@gmail.com <borodichalexander97@gmail.com>
2024-01-19 10:14:19 -0700

    feat(task-04): implement copy files

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -7,8 +7,20 @@ async function copyDir(basePath, dirName) {
   const POSTFIX_NAME = 'copy';
   const copyDirName = dirName.concat('-', POSTFIX_NAME);
   const copyDirPath = path.join(basePath, copyDirName);
+  const baseDirPath = path.join(basePath, dirName);
 
   await fs.mkdir(copyDirPath, { recursive: true });
+
+  const dirContent = await fs.readdir(baseDirPath, { withFileTypes: true });
+
+  for (const direntInstance of dirContent) {
+    if (direntInstance.isFile()) {
+      await fs.copyFile(
+        path.join(baseDirPath, direntInstance.name),
+        path.join(copyDirPath, direntInstance.name)
+      );
+    }
+  }
 }
 
 copyDir(__dirname, FOLDER_NAME).catch(console.error);
