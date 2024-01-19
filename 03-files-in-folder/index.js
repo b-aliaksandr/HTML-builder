9b802f3061462aad927f9e0571c129662090a826
barodzichaliaskandr@gmail.com <borodichalexander97@gmail.com>
2024-01-19 02:16:53 -0700

    refactor(task-03): rewrote to async/await

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -3,22 +3,26 @@ const path = require('node:path');
 const { stdout } = require('node:process');
 
 const FOLDER_NAME = 'secret-folder';
-const INSIDE_FOLDER_PATH = path.join(__dirname, FOLDER_NAME);
+const FOLDER_PATH = path.join(__dirname, FOLDER_NAME);
 
-async function logFileInfoToWritableStream(direntInstance, writableStream) {
+async function logFileInfoToOutput(direntInstance, output) {
   if (direntInstance.isFile()) {
     const [fileName, fileExt] = direntInstance.name.split('.');
     const { size: fileSize } = await fs.stat(path.join(direntInstance.path, direntInstance.name));
     const fileInfo = `${fileName} - ${fileExt} - ${fileSize}\n`;
-    writableStream.write(fileInfo);
+    output.write(fileInfo);
   }
 }
 
-const filesPromise = fs.readdir(INSIDE_FOLDER_PATH, { withFileTypes: true });
-filesPromise
-  .then((files) => {
-    for (const direntInstance of files) {
-      logFileInfoToWritableStream(direntInstance, stdout);
-    }
-  })
-  .catch((err) => console.error(err));
+async function readFilesInFolder(pathFolder) {
+  const files = await fs.readdir(pathFolder, { withFileTypes: true });
+  for (const file of files) {
+    logFileInfoToOutput(file, stdout);
+  }
+}
+
+try {
+  readFilesInFolder(FOLDER_PATH);
+} catch (err) {
+  console.error(err);
+}
