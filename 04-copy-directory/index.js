4c38cf4bd5b5ad4dc46ea48a063b560f84e5ded0
barodzichaliaskandr@gmail.com <borodichalexander97@gmail.com>
2024-01-22 03:28:35 -0700

    refactor(task-04): add copyDir to utils

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,27 +1,5 @@
-const fs = require('node:fs/promises');
-const path = require('node:path');
-const { createOrRecreateDir } = require('./utils');
+const { copyDir } = require('./utils');
 
 const FOLDER_NAME = 'files';
-
-async function copyDir(basePath, dirName) {
-  const POSTFIX_NAME = 'copy';
-  const copyDirName = dirName.concat('-', POSTFIX_NAME);
-  const copyDirPath = path.join(basePath, copyDirName);
-  const baseDirPath = path.join(basePath, dirName);
-
-  await createOrRecreateDir(copyDirPath);
-
-  const dirContent = await fs.readdir(baseDirPath, { withFileTypes: true });
-
-  for (const direntInstance of dirContent) {
-    if (direntInstance.isFile()) {
-      await fs.copyFile(
-        path.join(baseDirPath, direntInstance.name),
-        path.join(copyDirPath, direntInstance.name)
-      );
-    }
-  }
-}
 
 copyDir(__dirname, FOLDER_NAME).catch(console.error);
--- 04-copy-directory/utils.js
+++ 04-copy-directory/utils.js
@@ -1,4 +1,5 @@
-const fs = require('fs/promises');
+const fs = require('node:fs/promises');
+const path = require('node:path')
 
 async function createOrRecreateDir(folderPath) {
   try {
@@ -9,6 +10,27 @@ async function createOrRecreateDir(folderPath) {
   }
 }
 
+async function copyDir(basePath, dirName) {
+  const POSTFIX_NAME = 'copy';
+  const copyDirName = dirName.concat('-', POSTFIX_NAME);
+  const copyDirPath = path.join(basePath, copyDirName);
+  const baseDirPath = path.join(basePath, dirName);
+
+  await createOrRecreateDir(copyDirPath);
+
+  const dirContent = await fs.readdir(baseDirPath, { withFileTypes: true });
+
+  for (const direntInstance of dirContent) {
+    if (direntInstance.isFile()) {
+      await fs.copyFile(
+        path.join(baseDirPath, direntInstance.name),
+        path.join(copyDirPath, direntInstance.name)
+      );
+    }
+  }
+}
+
 module.exports = {
   createOrRecreateDir,
+  copyDir,
 }
