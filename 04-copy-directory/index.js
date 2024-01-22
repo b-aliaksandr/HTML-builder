0223edc1e71ac5d9992bce38962e6af01511540a
barodzichaliaskandr@gmail.com <borodichalexander97@gmail.com>
2024-01-22 01:45:03 -0700

    refactor(task-04): add node prefix

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,5 +1,5 @@
-const fs = require('fs/promises');
-const path = require('path');
+const fs = require('node:fs/promises');
+const path = require('node:path');
 const { createOrRecreateDir } = require('./utils');
 
 const FOLDER_NAME = 'files';
